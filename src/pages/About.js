ecbef64c5d9fee91c8032fb681f4c1826e0854e8
hipst3rsanta <connah.trotman00@gmail.com>
2025-06-04 18:48:12 +1000

    Remove resume section

--- src/pages/About.js
+++ src/pages/About.js
@@ -103,19 +103,6 @@ export default function AboutPage() {
                     </div>
                   </div>
                 )
-              },
-              {
-                label: 'run resume.pdf',
-                content: (
-                  <a
-                    href="/ConnahTrotman_Resume.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block px-4 py-2 mt-2 border border-[#39FF14] rounded bg-black text-[#39FF14] text-sm hover:bg-[#39FF14] hover:text-black transition-all shadow-[0_0_5px_#39FF14] hover:shadow-[0_0_10px_#39FF14]"
-                  >
-                    Open Résumé →
-                  </a>
-                )
               }
             ].map(({ label, content }, i) => (
               <div key={label} className="space-y-4 mb-12 bg-[#0c0c0c]/90 p-5 rounded-md border border-[#39FF14]/10 shadow-inner group transition-all">
